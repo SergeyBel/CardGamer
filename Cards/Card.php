1ee392b4954e170320b9c64e0332c2241a49680b
SergeyBel <belovseregas@gmail.com>
2017-07-01 16:29:47 +0300

    Imperove CArd class

--- Cards/Card.php
+++ Cards/Card.php
@@ -2,6 +2,21 @@
 
 class Card
 {
+ 
+
   public $suit; // 1...3
   public $value;  // 6..14
+  
+  const CARD_SUIT_MIN = 1;
+  const CARD_SUIT_MAX = 3;
+  const CARD_VAL_MIN = 6;
+  const CARD_VAL_MAX = 14;
+
+  function isCardCorrect()
+  {
+    return $this->suite >= $this::CARD_SUIT_MIN && $this->suit <= $this::CARD_SUIT_MAX &&
+          $this->value >= $this::CARD_VAL_MIN && $this->value <= $this::CARD_VAL_MAX;
+  }
+
+
 }
